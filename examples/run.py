bfb5f335b859d13d7c1c5017e3bb7e3888dc3ee4
Andres Hermosilla <an2dres5m@gmail.com>
2017-10-22 23:09:57 -0700

    Moved modules into directory to make pip installable

--- examples/run.py
+++ examples/run.py
@@ -2,8 +2,8 @@ from __future__ import print_function
 import pprint
 import sys
 sys.path.insert(0, '..')
-from model import OpenvasObject
-from wizard import ScanWizard
+from openvasup.model import OpenvasObject
+from openvasup.wizard import ScanWizard
 
 # Start openvas with Docker to get things doing quick!
 # `docker run -d -p 443:443 -p 9390:9390 --name openvas mikesplain/openvas`
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='openvas-up',
+      version='1.0',
+      description='Taking OpenVAS clients & automation to the next label',
+      url='http://github.com/rezen/openvas-up',
+      author='Andres Hermosilla',
+      license='MIT',
+      packages=['openvasup'],
+      zip_safe=False)
