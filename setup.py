2990c37d006009e151284cc0bb904b54a3ed3a15
Andres Hermosilla <an2dres5m@gmail.com>
2017-10-29 15:35:38 -0700

    Bump version to 1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='openvas-up',
-      version='1.0',
+      version='1.1',
       description='Taking OpenVAS clients & automation to the next label',
       url='http://github.com/rezen/openvas-up',
       author='Andres Hermosilla',
